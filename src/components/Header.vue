d3c15213c26886867428552a3a35e2c25c86b924
Edouard-Hashimoto <edouard.hashimoto@edu.univ-fcomte.fr>
2024-10-23 13:35:30 +0200

    Mise a jour graph

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-gray-800 text-white p-4 shadow-md">
+    <div class="container mx-auto flex justify-between items-center">
+      <h1 class="text-3xl font-bold">SAE303</h1>
+      <nav>
+        <ul class="flex space-x-4">
+          <li>
+            <RouterLink to="/graphique1" class="hover:text-gray-400 transition duration-300">Graph1</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/graphique2" class="hover:text-gray-400 transition duration-300">Graph2</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/graphique3" class="hover:text-gray-400 transition duration-300">Graph3</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/equipes" class="hover:text-gray-400 transition duration-300">test</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
