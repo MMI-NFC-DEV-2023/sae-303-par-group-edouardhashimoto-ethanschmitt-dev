03e872d3aaed1dd406f30713951d27f4a3ff26c4
Edouard-Hashimoto <edouard.hashimoto@edu.univ-fcomte.fr>
2024-10-23 14:39:23 +0200

    Footer

---
+++ src/components/footer.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <footer class="footer bg-gray-800 text-white py-6">
+    <div class="container mx-auto text-center">
+      <p class="mb-2">Ethan Schmitt, Edouard Hashimoto</p>
+      <p class="mb-4">&copy; 2024 Projet réalisée dans le cadre d'une SAE</p>
+    </div>
+  </footer>
+</template>
